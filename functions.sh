c0a43c28d8880648573cd4557a6b595b9083ee1c
Ari Zerner <ari@mgemi.com>
2016-08-12 09:27:07 -0400

    Added documentation to functions

--- functions.sh
+++ functions.sh
@@ -1,20 +1,29 @@
+# Usage: clearhistory
+# Clears bash history
 function clearhistory() {
     clear
     cat /dev/null > ~/.bash_history
     history -cw
 }
 
+# Usage: clean
+# Clears Downloads, Trash, and bash history
 function clean() {
     rm -rf ~/Downloads/ ~/.Trash/
     clearhistory
 }
 
+# Usage: sclean
+# Securely clears Downloads, Trash, and bash history
 function sclean() {
     rm -fP .bash_history
     rm -rfP ~/Downloads/ ~/.Trash/
     clearhistory
 }
 
+# Usage: csssh [host]
+# SSHs into a CS@RIT computer
+# host defaults to siren
 function csssh() {
     if [ $# == 0 ]; then
         ssh -Y afz8559@siren.cs.rit.edu
@@ -23,6 +32,9 @@ function csssh() {
     fi
 }
 
+# Usage: csftp [host]
+# FTPs to a CS@RIT computer
+# host defaults to siren
 function csftp() {
     if [ $# == 0 ]; then
         sftp afz8559@siren.cs.rit.edu
@@ -31,10 +43,16 @@ function csftp() {
     fi
 }
 
+# Usage: ssh-copy-id hostname
+# Copies SSH public key from local computer to a remote host
 function ssh-copy-id() {
     cat ~/.ssh/id_rsa.pub | ssh $1 "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
 }
 
+# Usage: hash algorithm file [expected]
+# Usage: hash -m algorithm message [expected]
+# Hashes a message or file using the given algorithm
+# If expected is provided, compares the hash to expected
 function hash() {
     if [[ $1 == "-m" ]]
     then
@@ -74,6 +92,10 @@ function hash() {
     fi
 }
 
+# Usage: checkmerge src dest
+# Shortcut for:
+#   git checkout dest
+#   git merge src
 function checkmerge() {
   if [ $# -eq 2 ]
   then
@@ -84,6 +106,8 @@ function checkmerge() {
   fi
 }
 
+# Usage: findtext text [dir]
+# Recursively finds text in files in the provided or current directory
 function findtext() {
   if [ $# -eq 0 -o $# -gt 2 ]
   then
