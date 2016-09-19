907b8b8d4ce1a8f5e1d85f1dfe83fb6705bb1a25
Ari Zerner <ari@mgemi.com>
2016-09-19 13:32:44 -0400

    Changed price algorithm in BTCTrade

--- scripts/BTCTrade.py
+++ scripts/BTCTrade.py
@@ -1,5 +1,10 @@
 import urllib, json, sys
 
+ask_flat_fee = 8
+ask_value_fee = 1.02
+bid_flat_fee = 8
+bid_value_fee = 1.02
+
 # get the BTC price from Coinbase
 def get_price():
     url = "https://api.coinbase.com/v2/prices/spot"
@@ -18,12 +23,9 @@ def btc_string(amount):
 
 def main(argv):
     price = get_price()
-    ask = price * 1.04
-    bid = price / 1.00
     if len(argv) == 1:
-        print 'Ask: %s for %s' % (dollar_string(ask), btc_string(1))
-        print 'Bid: %s for %s' % (dollar_string(bid), btc_string(1))
-    elif len(argv) == 2:
+        argv.append('B1')
+    if len(argv) == 2:
         amount = argv[1]
         dollars = True
         if amount.startswith('B'):
@@ -32,15 +34,17 @@ def main(argv):
         try:
             amount = float(amount)
             if dollars:
-                print 'Ask: %s for %s' % (dollar_string(amount),
-                        btc_string(amount / ask))
-                print 'Bid: %s for %s' % (dollar_string(amount),
-                        btc_string(amount / bid))
+                print 'Ask: %s for %s' % (dollar_string(amount),\
+                btc_string((amount - ask_flat_fee) / price / ask_value_fee))
+                print 'Bid: %s for %s' % (dollar_string(amount),\
+                btc_string((amount + bid_flat_fee) / price * bid_value_fee))
             else:
-                print 'Ask: %s for %s' % (dollar_string(amount * ask),
-                        btc_string(amount))
-                print 'Bid: %s for %s' % (dollar_string(amount * bid),
-                        btc_string(amount))
+                print 'Ask: %s for %s' %\
+                (dollar_string(amount * price * ask_value_fee + ask_flat_fee),\
+                btc_string(amount))
+                print 'Bid: %s for %s' %\
+                (dollar_string(amount * price / bid_value_fee - bid_flat_fee),\
+                btc_string(amount))
         except ValueError:
             print 'Amount must be a number, optionally preceeded by a B.'
     else:
