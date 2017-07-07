8851ed36bfc10d41a640f3d09a74679195598354
Ari Zerner <ari@mgemi.com>
2017-07-07 16:07:27 -0400

    Added gittree alias

--- aliases.sh
+++ aliases.sh
@@ -10,6 +10,8 @@ alias master="git checkout master"
 alias dev="git checkout development; git merge master"
 alias gcc="gcc -std=c99 -Wall -Wextra -pedantic"
 alias ip="curl ifconfig.co"
+alias gittree="git log --graph --full-history --all --color \
+  --pretty=format:\"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s\""
 
 # see https://github.com/nvbn/thefuck
 eval "$(thefuck --alias)"
